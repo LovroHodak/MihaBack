91bd5ebb5a002494dedddb9926b470c25542bc32
lovro <hodak.lovro@gmail.com>
2021-02-04 17:42:45 +0100

    getProducts, getProductId done

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const ProductModel = require("../models/product.model");
 
-router.get("/", (req, res) => {
+router.get("/products", (req, res) => {
   ProductModel.find()
     .then((products) => {
       res.status(200).json(products);
@@ -12,5 +12,21 @@ router.get("/", (req, res) => {
       res.status(500).json({message: 'Server Error'})
   })
 });
+
+/* req.params { id: '601bc4ee27cdf74cf4468064' }
+server return db-id */
+router.get('/products/:id', (req, res) => {
+  ProductModel.findById(req.params.id)
+   .then((response) => {
+     console.log('req.params', req.params)
+        res.status(200).json(response)
+   })
+   .catch((err) => {
+        res.status(500).json({
+             error: 'Something went wrong',
+             message: err
+        })
+   }) 
+})
 
 module.exports = router;
