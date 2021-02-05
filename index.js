8b119bf177ecc0f6a1b1619fd4a9f566092c206e
lovro <hodak.lovro@gmail.com>
2021-02-05 19:04:59 +0100

    get, getID, patchID... new model

--- index.js
+++ index.js
@@ -4,12 +4,28 @@ require('./config/db.config')
 const express = require('express')
 const app = express()
 
+
+//ENABLE FRONT+BACK
 const cors = require('cors')
 app.use(cors({
   credentials: true, 
   origin: ['http://localhost:3000']
 }))
 
+
+//REQ.BODY + BODY.PARSER => to get serverside console.log!! ALWAYS
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+//Use body parser. To be able parse post request information
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()) //crucial for post requests from client
+
+
+//ROUTES
 const productRoutes = require('./routes/product.routes')
 app.use('/api', productRoutes)
 
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -3,30 +3,81 @@ const router = express.Router();
 const ProductModel = require("../models/product.model");
 
 router.get("/products", (req, res) => {
+  console.log('req.body GET', req.body)
   ProductModel.find()
     .then((products) => {
       res.status(200).json(products);
     })
     .catch((error) => {
-      console.error(error)
-      res.status(500).json({message: 'Server Error'})
-  })
+      console.error(error);
+      res.status(500).json({ message: "Server Error" });
+    });
 });
 
 /* req.params { id: '601bc4ee27cdf74cf4468064' }
-server return db-id */
-router.get('/products/:id', (req, res) => {
+server always returns db-id */
+router.get("/products/:id", (req, res) => {
   ProductModel.findById(req.params.id)
-   .then((response) => {
-     console.log('req.params', req.params)
-        res.status(200).json(response)
-   })
-   .catch((err) => {
-        res.status(500).json({
-             error: 'Something went wrong',
-             message: err
-        })
-   }) 
-})
+    .then((response) => {
+      console.log("req.paramsGET", req.params);
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: "Something went wrong",
+        message: err,
+      });
+    });
+});
+
+router.patch("/products/:id", (req, res) => {
+  let id = req.params.id
+  console.log("req.paramsPATCH", req.params);
+  console.log("req.params.id", req.params.id);
+  console.log('req.body1', req.body)
+ 
+  console.log('req.body2', req.body)
+  console.log('req.body.item.nrOfItems -1', req.body.nrOfItems - 1)
+
+  
+  ProductModel.findByIdAndUpdate(id, {
+    $set: { nrOfItems: req.body.nrOfItems},
+  })
+    .then((response) => {
+      res.status(200).json(response);
+      console.log('edited')
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: "Something went wrong",
+        message: err,
+      });
+    });
+});
+
+
+/* router.patch("/products", (req, res) => {
+  
+  console.log('req.body', req.body)
+  let item = req.body.products.map((item) => {
+    return item.nrOfItems
+  })
+  console.log('req.body.products', req.body.products)
+  console.log('item', item)
+  let updatedProducts = req.body.products
+  ProductModel.updateMany({}, {$set: [item.map((prod) => prod)]})
+    .then((response) => {
+      res.status(200).json(response);
+      console.log('response is:', response)
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: "Something went wrong",
+        message: err,
+      });
+    });
+}); */
 
 module.exports = router;
